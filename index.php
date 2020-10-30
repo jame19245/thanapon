3990a4d725fe7d1af4e9c9cc99f34ba568618301
jame19245 <73687503+jame19245@users.noreply.github.com>
2020-10-30 17:28:35 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,23 @@
-<?php
-echo "<h2>PHP is Fun!</h2>";
-echo "Hello world!<br>";
-?>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>63070226-ITF</title>
+    </head>
+    <body>
+        <center>
+        <h1>Thanapon Poonwattansombat</h1>
+        <h1>James</h1>
+        <p></p><img src='cat11.jpg'></p>
+        <p></p><a href="https://www.it.kmitl.ac.th/th/">IT KMITL</a><p>
+        <img src="otter.jpg">
+        <h3>มีเด็กคนหนึ่งแม่สั่งให้ไปซื้อขนมปังตอนเช้าทุกๆวัน มีอยู่วันหนึ่งเด็กคนนั้นไปซื้อขนมปังเช่นทุกวัน<br>
+            เด็ก : ทำไมวันนี้ขนมปังเบาจังครับ<br>
+            คนขาย : ลุงใส่น้อยๆหนูจะได้ไม่หนักไง<br>
+            เด็กจึงให้เงินคนขายไป 3 บาท<br>
+            คนขาย : ทำไมหนูให้เงินลุงแค่ 3 บาท ล่ะ ปกติต้องให้ 5 บาทไม่ใช่เหรอ<br>
+            เด็ก : ก็ลุงจะได้นับเร็วๆไงครับ </h3>
+        </center>
+        <br>
+        </ul>
+    </body>
+</html>
