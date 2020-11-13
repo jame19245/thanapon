c688dac173038cd5e55195723c41ad3f60bae73f
jame19245 <73687503+jame19245@users.noreply.github.com>
2020-11-13 23:45:09 +0700

    Update edit.php

--- edit.php
+++ edit.php
@@ -13,7 +13,7 @@ $comment = $_POST['comment'];
 $link = $_POST['link'];
 
 
-$sql = "UPDATE guestbook (name , Comment , Link) SET ('$name', '$comment', '$link')";
+$sql = "UPDATE guestbook SET comment='$comment' WHERE name='$name";
 
 
 if (mysqli_query($conn, $sql)) {
