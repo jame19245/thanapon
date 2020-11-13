ccdb81ccf219075790924bac92b1482fe2babc3c
jame19245 <73687503+jame19245@users.noreply.github.com>
2020-11-13 21:13:13 +0700

    Update show.php

--- show.php
+++ show.php
@@ -27,6 +27,7 @@ while($Result = mysqli_fetch_array($res))
     <td><?php echo $Result['Name'];?></div></td>
     <td><?php echo $Result['Comment'];?></td>
     <td><?php echo $Result['Link'];?></td>
+    <td><a href="edit.php"></td>
   </tr>
 <?php
 }
