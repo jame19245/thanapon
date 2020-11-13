d6b3830f69ebce7d1a944d27d97e5feaf6579cd7
jame19245 <73687503+jame19245@users.noreply.github.com>
2020-11-13 20:38:13 +0700

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,7 +1,7 @@
 <?php
 
 $conn = mysqli_init();
-mysqli_real_connect($conn, 'panwit.mysql.database.azure.com', 'panwit@panwit', '123456789', 'itflab', 3306);
+mysqli_real_connect($conn, 'thanapon-226.mysql.database.azure.com', 'Jame@thanapon-226', 'thanapon-19245', 'ITFLab', 3306);
 if (mysqli_connect_errno($conn))
 {
     die('Failed to connect to MySQL: '.mysqli_connect_error());
