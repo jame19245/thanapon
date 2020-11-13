70814f408c3a6e32409c08c7f85cbfa187ceba36
jame19245 <73687503+jame19245@users.noreply.github.com>
2020-11-13 23:23:18 +0700

    Update delete.php

--- delete.php
+++ delete.php
@@ -13,7 +13,7 @@ $comment = $_POST['comment'];
 $link = $_POST['link'];
 
 
-$sql = "DELETE FROM guestbook WHERE (name='$name', Comment='$comment')";
+$sql = "DELETE FROM guestbook WHERE name='$name'";
 
 
 
