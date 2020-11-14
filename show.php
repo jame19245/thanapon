ca3552dcb3f09158ba858fc611d1a941f4d21f5e
jame19245 <73687503+jame19245@users.noreply.github.com>
2020-11-14 11:15:57 +0700

    Update show.php

--- show.php
+++ show.php
@@ -1,6 +1,11 @@
 <html>
 <head>
 <title>ITF Lab</title>
+<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 </head>
 <body>
 <?php
@@ -12,28 +17,30 @@ if (mysqli_connect_errno($conn))
 }
 $res = mysqli_query($conn, 'SELECT * FROM guestbook');
 ?>
-<table width="600" border="1" ><center>
-  <tr>
-    <th width="100"> <div align="center">Name</div></th>
-    <th width="350"> <div align="center">Comment </div></th>
-    <th width="150"> <div align="center">Action</div></th>
-  </tr>
-<?php
-while($Result = mysqli_fetch_array($res))
-{
-?>
-  <tr>
-    <td><?php echo $Result['name'];?></div></td>
-    <td><?php echo $Result['Comment'];?></td>
-    <td><a href="form_edit.html"><input type="submit" value="Edit"></a><a href="form_delete.html"><input type="submit" value="Delete"></a></td>
-  </tr>
-<?php
-}
-?>
-</center></table>
-<a href="form_insert.html"><input type="submit" value="Insert"></a>
-<?php
-mysqli_close($conn);
-?>
+<div class="container">
+  <table width="600" border="1" class="table table-hover"><center>
+    <tr>
+      <th width="100"> <div align="center">Name</div></th>
+      <th width="350"> <div align="center">Comment </div></th>
+      <th width="150"> <div align="center">Action</div></th>
+    </tr>
+  <?php
+  while($Result = mysqli_fetch_array($res))
+  {
+  ?>
+    <tr>
+      <td><?php echo $Result['name'];?></div></td>
+      <td><?php echo $Result['Comment'];?></td>
+      <td><a href="form_edit.html"><input type="submit" value="Edit"></a><a href="form_delete.html"><input type="submit" value="Delete"></a></td>
+    </tr>
+  <?php
+  }
+  ?>
+  </center></table>
+  <a href="form_insert.html"><input type="submit" value="Insert"></a>
+  <?php
+  mysqli_close($conn);
+  ?>
+</div>
 </body>
 </html>
