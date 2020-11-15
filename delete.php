aa22bd9be512b5a321ce703602b67672d3a638ea
jame19245 <73687503+jame19245@users.noreply.github.com>
2020-11-15 13:07:26 +0700

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,3 +1,18 @@
+<html>
+<head>
+<title>ITF Lab</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <style>
+  p.ex1 {
+    margin-top: 25px;
+  }
+  </style>
+</head>
+<body>
 <?php
 
 $conn = mysqli_init();
@@ -14,12 +29,17 @@ $sql = "DELETE FROM guestbook WHERE name='$name'";
 $sql = "DELETE FROM guestbook WHERE Comment='$comment'";
 
 
-
 if (mysqli_query($conn, $sql)) {
-    echo "New record created successfully";
+    echo "<center>Delete Successfully</center>";
   } else {
-    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    echo "<center>Error: </center>" . $sql . "<br>" . mysqli_error($conn);
   }
 
 mysqli_close($conn);
 ?>
+
+<div class="container">
+  <center><a href="show.php"><input type="button" value="Show" class="btn btn-info" class="ex1"></a></center>
+</div>
+</body>
+</html>
