7ba0b9edb8b04bc6258207529d3c8fbf4a07d0e5
jame19245 <73687503+jame19245@users.noreply.github.com>
2020-11-27 17:18:07 +0700

    Update inserttest.php

--- inserttest.php
+++ inserttest.php
@@ -27,8 +27,8 @@
     $name = $_POST['name'];
     $height = $_POST['height'];
     $weight = $_POST['weight'];
-    $bmi = $weight / ($height ** 2);
-
+    $cal = $weight / (($height / 100) ** 2);
+    $bmi = round($cal, 2);
 
     $sql = "INSERT INTO itf-test (name , height, weight, bmi) VALUES ('$name', '$height', '$weight', '$bmi')";
 
